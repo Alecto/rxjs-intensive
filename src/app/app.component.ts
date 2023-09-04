0df84ae47c1689634c2b8efd3608fa2cff39a523
Alecto <fomenkoandrey1978@gmail.com>
2023-09-04 19:28:52 +0300

    рассмотрели проблему на промисах, решили ее на генераторах

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +6,33 @@ import { Component } from '@angular/core';
   template: `<h1>Hello</h1>`,
   styles: [``]
 })
-export class AppComponent {
-  
+export class AppComponent implements OnInit {
+
+  ngOnInit(): void {
+
+    // const sequence = new Promise(resolve => {
+    //   let count = 1;
+    //   setInterval(() => {
+    //     resolve(count++);
+    //   }, 1000);
+    // });
+    // sequence.then(console.log);
+    // sequence.then(console.log);
+
+    // const sequence = function* iteratorFn() {
+    //   let count = 1;
+    //
+    //   while (true) {
+    //     yield count++;
+    //   }
+    // }();
+    //
+    // console.log(sequence.next().value);
+    // console.log(sequence.next().value);
+    // console.log(sequence.next().value);
+    // console.log(sequence.next().value);
+    // console.log(sequence.next().value);
+
+  }
+
 }
