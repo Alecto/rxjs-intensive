6858bc7f1c94c0cfe3d338d8dced7ea2d09b139f
Alecto <fomenkoandrey1978@gmail.com>
2023-09-04 19:17:45 +0300

    standalone component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  standalone: true,
+  template: `<h1>Hello</h1>`,
+  styles: [``]
 })
 export class AppComponent {
-  title = 'rxjs-intensive';
+  
 }
