cec51eb472608cac7ad5c93ba51978c209d2c388
Alecto <fomenkoandrey1978@gmail.com>
2023-09-06 20:03:25 +0300

    входные данные для задачи виджета слайдера

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,30 @@
 import { AfterViewInit, Component, OnInit } from '@angular/core';
-import { SwipeService } from './swipe.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  template: ``,
-  styles: [``]
+  template: `
+    <div class="container py-5">
+      <div class="slider">
+        <input #quality type="range" class="form-range" min="0" max="100" id="quality">
+        <input #rating type="range" class="form-range" min="0" max="100" id="rating">
+        <input #actual type="range" class="form-range" min="0" max="100" id="actual">
+      </div>
+    </div>
+  `,
+  styles: [`
+    .slider {
+      max-width: 400px;
+      padding: 30px;
+      border: 2px solid lightsteelblue;
+    }
+  `]
 })
 export class AppComponent implements OnInit, AfterViewInit {
 
-  constructor(private swipe: SwipeService) {}
+  constructor() {}
 
   ngOnInit(): void {
-    this.swipe.init();
   }
 
   ngAfterViewInit() {
