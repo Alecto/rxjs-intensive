f85950e7be23fcfd1cc2514014d2ce3aec7f5088
Alecto <fomenkoandrey1978@gmail.com>
2023-09-06 19:54:21 +0300

    swipes в компоненте

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,57 @@
-import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { debounceTime, fromEvent, map } from 'rxjs';
+import { AfterViewInit, Component, Inject, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { fromEvent, map, merge, Observable, zip } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  template: `<input type="text" #input>`,
+  template: ``,
   styles: [``]
 })
 export class AppComponent implements OnInit, AfterViewInit {
 
-  @ViewChild('input')
-  input!: ElementRef;
+  constructor(@Inject(DOCUMENT) private document: Document) {}
 
   ngOnInit(): void {
 
   }
 
   ngAfterViewInit() {
-    const el = this.input.nativeElement as HTMLInputElement;
 
-    fromEvent<InputEvent>(el, 'input')
-      .pipe(
-        debounceTime(1000),
-        map(e => (e.target as HTMLInputElement).value)
+    this.swipe(zip(
+      this.getX(
+        fromEvent<TouchEvent>(document, 'touchstart'),
+        fromEvent<MouseEvent>(document, 'mousedown')
+      ),
+      this.getX(
+        fromEvent<TouchEvent>(document, 'touchend'),
+        fromEvent<MouseEvent>(document, 'mouseup')
       )
-      .subscribe(value => {
-        console.log(value);
-      });
+    )).subscribe(direction => {
+      if (direction < 100) {
+        console.log('swipe left');
+        return;
+      }
+      console.log('swipe right');
+    });
+
+  }
+
+  private getX(source1$: Observable<TouchEvent>, source2$: Observable<MouseEvent>): Observable<number> {
+    return merge(source1$, source2$).pipe(
+      map((event: MouseEvent | TouchEvent) => {
+        if (event instanceof TouchEvent) {
+          return event.changedTouches[0].clientX;
+        }
+        return event.clientX;
+      })
+    );
+  }
+
+  private swipe(source$: Observable<[number, number]>) {
+    return source$.pipe(
+      map(([x, y]) => y - x)
+    );
   }
 
 }
